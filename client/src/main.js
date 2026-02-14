82194dbbd9728fe50d4509f6f1e2d5de8555dade
Demo <nicodemopavesi@gmail.com>
2026-02-14 15:33:59 +0000

    pusho eh

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,43 @@
 import BootScene from './scenes/BootScene.js';
+import './WebSocketClient.js';
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Generazione ID utente persistente
+let userId = localStorage.getItem("userId");
+
+if (!userId) {
+  // genera un ID unico e sicuro con timestamp
+  userId = ${crypto.randomUUID()};
+  localStorage.setItem("userId", userId);
+}
+
+console.log("UserID locale:", userId);
+
+// Connessione al server
+// const socket = io(); (questa avviene in WebSocketClient.js dopo che l'utente ha scelto il nome)
+
+// Schermate
+const lobbyScreen = document.getElementById("lobbyScreen");
+const roomScreen = document.getElementById("roomScreen");
+const gameScreen = document.getElementById("gameScreen");
+const userMobileWarning = document.getElementById("userMobileWarning");
+const userIdWarning = document.getElementById("userIdWarning");
+
+// serve qualcosa che non permetta di far eseguire questa funzione da console client (F12)
+// da vedere come spostare sta roba lato server
+// Cambio schermate
+function showScreen(screen) {
+  userMobileWarning.classList.add("hidden");
+  userIdWarning.classList.add("hidden");
+  lobbyScreen.classList.add("hidden");
+  roomScreen.classList.add("hidden");
+  gameScreen.classList.add("hidden");
+
+  screen.classList.remove("hidden");
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const imageCache = new Map();
 const config = {
